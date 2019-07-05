6392376d74baec98944cb15fbfe8af2dd97a0e87
toeysp130 <49556609+toeysp130@users.noreply.github.com>
2019-07-05 22:34:05 +0700

    Create lab2-EX_2-3.py

---
+++ lab2-EX_2-3.py
@@ -0,0 +1,10 @@
+'''******************************************
+6206021612059 Watcharakorn  Choptum
+section C  IT
+******************************************'''
+A = int (input("Enter amount : "))
+R = float (input("Enter rate : "))
+Y = int (input("Enter year : "))
+fv = A*(1+R/100)**Y
+
+print ("Future value = ",fv)
