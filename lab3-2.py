7433c8a646d37231dc8642777b52b32e6aa56010
toeysp130 <49556609+toeysp130@users.noreply.github.com>
2019-07-12 10:32:38 +0700

    Create lab3-2.py

---
+++ lab3-2.py
@@ -0,0 +1,14 @@
+#watcharakorn#
+
+
+num1 = int( input("Enter value number 1 :"))
+num2 = int( input("Enter value number 2 :"))
+num3 = int( input("Enter value number 3 :"))
+
+MinValue = min(num1, num2, num3)
+MaxValue = max(num1, num2, num3)
+
+print()
+print("Your Enter Number :",num1, num2, num3)
+print("maxvalue : " , MaxValue)
+print("minvalue : " , MinValue)
