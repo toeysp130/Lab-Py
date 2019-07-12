82a12a95d9a03a8fba58d9e0881c9a1d7810dff1
toeysp130 <49556609+toeysp130@users.noreply.github.com>
2019-07-12 10:32:36 +0700

    Create lab3-1.py

---
+++ lab3-1.py
@@ -0,0 +1,12 @@
+#watcharakorn#
+Sum = 0
+Sum += int( input ("Enter number 1 : "))
+Sum += int( input ("Enter number 2 : "))
+Sum += int( input ("Enter number 3 : "))
+Sum += int( input ("Enter number 4 : "))
+
+Average = Sum / 4
+
+print ()
+print ("sumation", Sum)
+print ("average" , Average)
