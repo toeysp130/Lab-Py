1174a31648a79e827cf90f8f7710878cf7428b31
toeysp130 <49556609+toeysp130@users.noreply.github.com>
2019-07-12 11:06:43 +0700

    Create lab3-5.py

---
+++ lab3-5.py
@@ -0,0 +1,13 @@
+#watcharakorn#
+import random
+
+a = random.randint( 1,10)
+print ("a = " ,a)
+b = random.random()
+print ("b = " ,b)
+c = random.uniform(1.5, 6.5)
+print ("c = " ,c)
+d = random.choice('AEIOU')
+print ("d = " ,d)
+e = random.randrang(10 ,101 ,10)
+print ("e = " ,e)
