c4971b26d1c3a9a55867b0eea4ffb1c9a596f932
toeysp130 <49556609+toeysp130@users.noreply.github.com>
2019-07-12 10:32:40 +0700

    Create lab3-3.py

---
+++ lab3-3.py
@@ -0,0 +1,10 @@
+#watcharakorn#
+decimal = float( input("Enter float number( 5 decimal) : "))
+print()
+print("Float number : ", round(decimal))
+print("Float number (1 decimal) : ", round(decimal, 1))
+print("Float number (2 decimal) : ", round(decimal, 2))
+print("Float number (3 decimal) : ", round(decimal, 3))
+print("Float number (4 decimal) : ", round(decimal, 4))
+print("Float number (5 decimal) : ", round(decimal, 5))
+
