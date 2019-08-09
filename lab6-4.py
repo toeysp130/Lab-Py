8aa9a80b6eb547d64701364a3edbeaeca87603a0
toeysp130 <49556609+toeysp130@users.noreply.github.com>
2019-08-09 10:44:13 +0700

    Create lab6-4.py

---
+++ lab6-4.py
@@ -0,0 +1,9 @@
+Message = ""
+Max = 5
+Count = 1
+while Count <= Max:
+    S = input(f"Enter string #{Count}: ")
+    Message += S + "\n"
+    Count += 1
+print("\nPrint Your string enter : ")
+print(Message)
