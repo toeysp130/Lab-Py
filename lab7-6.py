a48f05b958d9be58baddb3882b21586b6d224ad5
toeysp130 <49556609+toeysp130@users.noreply.github.com>
2019-08-30 10:49:58 +0700

    Create lab7-6.py

---
+++ lab7-6.py
@@ -0,0 +1,10 @@
+def print_triangle(num = 5):
+    for n in range (1,num+1):
+        text = "#" * n
+        print(text)
+print()
+print_triangle()
+print()
+print_triangle(3)
+print()
+print_triangle(8)
